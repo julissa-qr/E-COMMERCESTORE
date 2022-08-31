b86aaa9b4b7f26bcef245e96c31a18b51b50b3db
Julissa Quiroz <julissa_qr@outlook.es>
2022-08-31 02:34:15 -0500

    30ago

--- store/src/app/features/catalog/ProductDetails.tsx
+++ store/src/app/features/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Divider, Grid, Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -34,7 +34,38 @@ export default function ProductDetails() {
          <Grid item xs={6}>
             <Typography variant="h3">{product.name}</Typography>
             <Divider sx={{mb: 2}} />
-            <Typography variant="h3" color='secondary'>{(product.price / 100).toFixed(2)}</Typography>
+            <Typography variant="h3" color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
+        <TableContainer>
+         <Table>
+            <TableBody>
+               <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>{product.name}</TableCell>
+               </TableRow>
+
+               <TableRow>
+                  <TableCell>Description</TableCell>
+                  <TableCell>{product.description}</TableCell>
+               </TableRow>
+
+               <TableRow>
+                  <TableCell>Type</TableCell>
+                  <TableCell>{product.type}</TableCell>
+               </TableRow>
+
+               <TableRow>
+                  <TableCell>Brand</TableCell>
+                  <TableCell>{product.brand}</TableCell>
+               </TableRow>
+
+               <TableRow>
+                  <TableCell>Quantiy in stock</TableCell>
+                  <TableCell>{product.quantityInStock}</TableCell>
+               </TableRow>
+            </TableBody>
+         </Table>
+        </TableContainer>
+        
          </Grid>
       </Grid>
    )
