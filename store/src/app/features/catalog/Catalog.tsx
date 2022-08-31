27dcbb1fbc5303e2df1e5b11e9495a0162f34de6
Julissa Quiroz <julissa_qr@outlook.es>
2022-08-31 00:42:03 -0500

    30ago

--- store/src/app/features/catalog/Catalog.tsx
+++ store/src/app/features/catalog/Catalog.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@mui/material";
 import { useState, useEffect } from "react";
 import { Product } from "../../models/product"
 import ProductList from "./ProductList";
--- store/src/app/features/catalog/ProductDetails.tsx
+++ store/src/app/features/catalog/ProductDetails.tsx
@@ -24,7 +24,7 @@ export default function ProductDetails(){
 
    return(
     <Typography variant="h2">
-       products detailsss
+       {product.id}
     </Typography>
    )
 }
