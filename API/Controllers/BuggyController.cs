d90ce714cadc0ac719d79811e10f8b227cea8bc3
Julissa Quiroz <julissa_qr@outlook.es>
2022-08-31 19:03:55 -0500

    31ago

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        //404
+        [HttpGet("not-Found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ProblemDetails{Title="Bad Request"});
+        }
+
+        //401
+        [HttpGet("unauthorised")]
+        public ActionResult GetUnauthorised()
+        {
+            return Unauthorized();
+        }
+        //400
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem1", "First Error");
+            ModelState.AddModelError("Problem2", "Second Error");
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("Server error");
+        
+        }
+    }
+}
