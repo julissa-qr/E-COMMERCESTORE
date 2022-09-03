6c0a2a6c4739f2e9c9fd0ab35cee0ca41186e154
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-03 00:52:18 -0500

    03sep

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    [Table("Baskets")]
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string CostumerId { get; set; }
+        public List<BasketItem> Items { get; set; } = new();
+
+        //if there is a product quant increase if not, the prod is added 
+        public void AddItem(Product product, int quantity)
+        {
+            if (Items.All(item => item.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem { Product = product, Quantity = quantity });
+            }
+            //adjust the quantity
+            var existingItem = Items.FirstOrDefault( item => item.ProductId == product.Id);
+            if(existingItem != null) existingItem.Quantity += quantity;
+        }
+        /*
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }*/
+
+
+    }
+}
