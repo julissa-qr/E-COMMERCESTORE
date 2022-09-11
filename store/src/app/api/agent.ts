0f926fa4d6d891999fc977e004ac08bf2abf4ae2
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-10 20:30:00 -0500

    ok

--- store/src/app/api/agent.ts
+++ store/src/app/api/agent.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
@@ -6,12 +6,12 @@ axios.defaults.withCredentials =true;
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-/*axios.interceptors.response.use(response => {
+axios.interceptors.response.use(response => {
     return response
 }, (error: AxiosError) => {
     console.log('caught by interceptor');
     return Promise.reject(error.response);
-})*/
+})
 
 const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
