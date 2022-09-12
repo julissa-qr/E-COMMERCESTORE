698d8f242f4cff39dfe0e391a50680785256bf9c
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-12 15:19:54 -0500

    ok

--- store/src/app/features/catalog/ProductCard.tsx
+++ store/src/app/features/catalog/ProductCard.tsx
@@ -3,6 +3,7 @@ import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, 
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import agent from "../../api/agent";
+import { useStoreContext } from "../../context/StoreContext";
 import { Product } from "../../models/product";
 
 interface Props {
@@ -13,12 +14,14 @@ interface Props {
 export default function ProductCard({ product }: Props) {
 
     const [loading, setLoading] = useState(false);
+    const {setBasket} = useStoreContext();
 
     function handleAddItem(productId: number) {
         setLoading(true);
         agent.Basket.addItem(productId)
+            .then(basket => setBasket(basket))
             .catch(error => console.log(error))
-            .finally(() => setLoading(false))
+            .finally(() => setLoading(false));
     }
 
 
