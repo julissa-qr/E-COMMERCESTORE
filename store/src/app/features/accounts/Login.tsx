096602b2e42a4dd4d35d83241067ad7d935691f5
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-13 16:41:57 -0500

    ok

--- store/src/app/features/accounts/Login.tsx
+++ store/src/app/features/accounts/Login.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
@@ -11,77 +9,85 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import agent from '../../api/agent';
+import { FieldValues, useForm } from 'react-hook-form';
+import { LoadingButton } from '@mui/lab';
 
 
 const theme = createTheme();
 
 export default function Login() {
-    const [values, setValues] = useState({
-        username: '',
-        password: ''
-    })
 
-  const handleSubmit = (event: any) => {
-    //console.log(values);
-    event.preventDefault(); //stop full page reload
-    agent.Account.login(values);
-  };
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm()
 
-  function handleInputChange(event: any)
-  {
-    const {name, value} = event.target;
-    setValues({...values, [name]: value}); //spread separator, tomar todas las propiedades en values
+  async function submitForm(data: FieldValues) {
+    try {
+      await agent.Account.login(data);
+    } catch(error){
+      console.log(error);
+    }
   }
-  
+  /*const [values, setValues] = useState({
+      username: '',
+      password: ''
+  })
+
+const handleSubmit = (event: any) => {
+  //console.log(values);
+  event.preventDefault(); //stop full page reload
+  agent.Account.login(values);
+};
+
+function handleInputChange(event: any)
+{
+  const {name, value} = event.target;
+  setValues({...values, [name]: value}); //spread separator, tomar todas las propiedades en values
+}*/
+
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component={Paper} maxWidth="sm" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', p: 4}}>
+      <Container component={Paper} maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', p: 4 }}>
         <CssBaseline />
-       
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              fullWidth
-              label="Username"
-              name="username"
-              autoFocus
-              onChange={handleInputChange}
-              value = {values.username}
-            />
-            <TextField
-              margin="normal"
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              onChange={handleInputChange}
-              value = {values.password}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link to='/register'>
-                    {"Don't have an account? Sign Up!"}
-                </Link>
-              </Grid>
+
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Username"
+            autoFocus
+            {...register('username', { required: 'Username required' })}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Password"
+            type="password"
+            {...register('password', { required: 'Password required' })}
+          />
+          <LoadingButton
+            loading={isSubmitting}
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </LoadingButton>
+          <Grid container>
+            <Grid item>
+              <Link to='/register'>
+                {"Don't have an account? Sign Up!"}
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
+        </Box>
       </Container>
     </ThemeProvider>
   );
