75594d8853a01ce9ff021ba0eb29b64298c45f66
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-18 01:42:02 -0500

    order entities and props

---
+++ API/Entities/Address.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Address
+    {
+        public string FullName  { get; set; }
+        public string Address1 { get; set; }
+    }
+}
