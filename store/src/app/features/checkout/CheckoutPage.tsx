58ad8e7155357b68bb69757d747c3c03ba8ae3fd
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-19 02:19:20 -0500

    create order ok

--- store/src/app/features/checkout/CheckoutPage.tsx
+++ store/src/app/features/checkout/CheckoutPage.tsx
@@ -6,6 +6,10 @@ import PaymentForm from "./PaymentForm";
 import Review from "./Review";
 import { yupResolver } from '@hookform/resolvers/yup';
 import { validationSchema } from "./checkoutValidation";
+import agent from "../../api/agent";
+import { useAppDispatch } from "../../store/configureStore";
+import { clearBasket } from "../basket/basketSlice";
+import { LoadingButton } from "@mui/lab";
 
 const steps = ['Shipping address', 'Review your order', 'Payment details'];
 
@@ -23,17 +27,38 @@ function getStepContent(step: number) {
 }
 
 export default function CheckoutPage() {
+
+    const [activeStep, setActiveStep] = useState(0);
+    const [orderNumber, setOrderNumber] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const dispatch = useAppDispatch();
+
+    const currentValidationSchema = validationSchema[activeStep];
+
     const methods = useForm({
         mode: 'all',
-        resolver: yupResolver(validationSchema)
+        resolver: yupResolver(currentValidationSchema)
     });
-    const [activeStep, setActiveStep] = useState(0);
 
-    const handleNext = (data: FieldValues) => {
-        if(activeStep === 0) {
-            console.log(data);
+    const handleNext = async (data: FieldValues) => {
+        const { nameOnCard, saveAddress, ...shippingAddress } = data;
+        if (activeStep === steps.length - 1) {
+            //console.log(data);
+            setLoading(true);
+            try {
+                const orderNumber = await agent.Orders.create({ saveAddress, shippingAddress });
+                setOrderNumber(orderNumber);
+                setActiveStep(activeStep + 1);
+                dispatch(clearBasket());
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+            }
+        } else {
+
+            setActiveStep(activeStep + 1);
         }
-        setActiveStep(activeStep + 1);
     };
 
     const handleBack = () => {
@@ -60,7 +85,7 @@ export default function CheckoutPage() {
                                 Thank you for your order.
                             </Typography>
                             <Typography variant="subtitle1">
-                                Your order number is #2001539. We have emailed your order
+                                Your order number is #{orderNumber}. We have emailed your order
                                 confirmation, and will send you an update when your order has
                                 shipped.
                             </Typography>
@@ -74,14 +99,15 @@ export default function CheckoutPage() {
                                         Back
                                     </Button>
                                 )}
-                                <Button
+                                <LoadingButton
+                                    loading={loading}
                                     disabled={!methods.formState.isValid}
                                     variant="contained"
                                     type='submit'
                                     sx={{ mt: 3, ml: 1 }}
                                 >
                                     {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                                </Button>
+                                </LoadingButton>
                             </Box>
                         </form>
                     )}
