636a327f917253ff28a363a4fd806018abb5c3a0
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-20 03:25:22 -0500

    homepage almost done

--- store/src/app/features/home/HomePage.tsx
+++ store/src/app/features/home/HomePage.tsx
@@ -1,9 +1,34 @@
 import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Slider from "react-slick";
 
-export default function HomePage(){
-   return(
-    <Typography variant="h2">
-        Home
-    </Typography>
-   )
+export default function HomePage() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    return (
+        <>
+            <Slider>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2016/11/22/19/08/hangers-1850082__340.jpg" alt="hero" style={{display: 'block', width: '100%', maxHeight: 500}}/>
+                </div>
+                <div>
+                    <img src="/" alt="hero" style={{display: 'block', width: '100%', maxHeight: 500}}/>
+                </div>
+                <div>
+                    <img src="/" alt="hero" style={{display: 'block', width: '100%', maxHeight: 500}}/>
+                </div>
+            </Slider>
+            <Box display='flex' justifyContent='center' sx={{p: 4}} >
+                <Typography variant='h1'>
+                    Welcome to StoreWare
+                </Typography>
+            </Box>
+        </>
+    )
 }
