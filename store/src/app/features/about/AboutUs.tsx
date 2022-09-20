ca9be1d0c39d59684d7c053c6edfa4df469985f8
JoseGonzalez-Hexaware <112432927+JoseGonzalez-Hexaware@users.noreply.github.com>
2022-09-20 10:51:15 -0500

    Se agrego el correo

--- store/src/app/features/about/AboutUs.tsx
+++ store/src/app/features/about/AboutUs.tsx
@@ -13,28 +13,27 @@ export default function AboutUs() {
     }
 
     return (
-        <Container>
-            <Typography gutterBottom variant="h2" > Errors for testingg </Typography>
-            <ButtonGroup fullWidth>
-                <Button variant="contained" onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))}>Test400Error</Button>
-                <Button variant="contained" onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>Test401Error</Button>
-                <Button variant="contained" onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>Test404Error</Button>
-                <Button variant="contained" onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>Test500Error</Button>
-                <Button variant="contained" onClick={getValidationError}>Test Validation</Button>
-            </ButtonGroup>
-            {validationErrors.length > 0 &&
-                <Alert severity="error">
-                    <AlertTitle>Validation Errors</AlertTitle>
-                    <List>
-                        {validationErrors.map(error =>(
-                            <ListItem key={error}>
-                                <ListItemText>{error}</ListItemText>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Alert>
-            
-            }
-        </Container>
+<div>
+  <meta charSet="utf-8" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+  <title>About Us card || Learning robo</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="style.css" />
+  <style dangerouslySetInnerHTML={{__html: "\n@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&display=swap');\n\n*{\n    box-sizing: border-box;\n    padding: 0;\n    margin: 0;\n}\nbody{\n    font-family: 'Playfair Display', serif;\n    background-color: #F0F0F0;\n\n\n    align-content: center;\n    min-height: 100vh;\n}\nsection{\n    display: grid;\n    grid-template-columns: 1fr 1fr;\n    min-height: 70vh;\n    width: 50vw;\n    margin: 0 auto;\n    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);\n    border-radius: 12px\n}\n.image{\n    background-color: #FFF;\n    display: flex;\n    border-radius: 12px 0 0 12px;\n}\n.image img{\nheight:50vh;\nmargin:50px auto\n}\n.content{\n    background-color: #000;\n    display: flex;\n    justify-content: center;\n    flex-direction: column;\n    align-items: center;\n    border-radius: 0  12px 12px 0;\n    color: #fff;\n}\n.content h2{\n    text-transform: uppercase;\n    font-size: 36px;\n    letter-spacing: 8px;\n    opacity: 0.9;\n}\n.content span{\n    height: 0.5px;\n    width: 80px;\n    background: #777;\n    margin: 30px 0;\n}\n.content p{\n    padding-bottom: 15px;\n    font-weight: 300;\n    opacity: 0.9;\n    width: 60%;\n    text-align: center;\n    margin: 0 auto;\n    line-height: 1.7;\n    color:#ffffff\n}\n.links{\n    margin: 15px 0;\n}\n.links li{\n    border: 2px solid #4158D0;\n    list-style: none;\n    border-radius: 5px;\n    padding: 10px 15px;\n    width: 160px;\n    text-align: center;\n}\n.links li a{\n    text-transform: uppercase;\n    color: #fff;\n    text-decoration: none;\n}\n.links li:hover{\n    border-color: #FFF300;\n}\n\n.vertical-line{\n    height: 30px;\n    width: 3px;\n    background: #C850C0;\n    margin: 0 auto;\n}\n.icons{\n    display: flex;\n    padding: 15px 0;\n}\n.icons li{\n    display: block;\n    padding: 5px;\n    margin: 5px;\n}\n.icons li i{\n    font-size: 26px;\n    opacity: 0.8;\n}\n.icons li i:hover{\n    color: #FF0000;\n    cursor: pointer;\n}\n\n\n/*****************/\n\n@media(max-width: 900px){\n    section{\n        grid-template-columns: 1fr;\n        width: 100%;\n        border-radius: none;\n    }\n    .image{\n        height: 100vh;\n        border-radius: none;\n    }\n    .content{\n        height: 100vh;\n        border-radius: none;\n    }\n    .content h2{\n        font-size: 20px;\n        margin-top: 50px;\n    }\n    .content span{\n        margin: 20px 0;\n    }\n    .content p{\n        font-size: 14px;\n    }\n    .links li a{\n        font-size: 14px;\n    }\n    .links{\n        margin: 5px 0;\n    }\n    .links li{\n        padding: 6px 10px;\n    }\n    .icons li i{\n        font-size: 15px;\n    }\n}\n.credit{\n    text-align: center;\n    color: #000;\n    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;\n  }\n  \n  .credit a{\n    text-decoration: none;\n    color:#000;\n    font-weight: bold;\n  } \n\n\n    " }} />
+  <section>
+    <div className="image">
+      <img src="https://i.postimg.cc/gjJB0L38/unnamed-1-1.png" />
+    </div>
+    <div className="content">
+      <h2>About Us</h2>
+      <span>{/* line here */}</span>
+      <p>We ensure customer satisfaction by adding value and honouring commitments at all times. We are committed to building shareholder value and maintaining high standards of corporate governance. We strive to be an eco-friendly organisation, inculcating good corporate citizenship.</p>
+      <ul className="links">
+        <li><a href="https://www.hexaware.com/about-us">Read More</a></li>
+      </ul>
+      
+    </div>
+  </section><br /><br />
+</div>
     )
 }
--- store/src/app/features/contact/ContactUs.tsx
+++ store/src/app/features/contact/ContactUs.tsx
@@ -1,4 +1,5 @@
-import { Button, ButtonGroup, Typography } from "@mui/material";
+
+import { Button, ButtonGroup, Typography, Grid, Card, CardContent, TextField } from "@mui/material";
 
 import { useAppDispatch, useAppSelector } from "../../store/configureStore";
 import { decrement, increment } from "./counterReducer";
@@ -8,21 +9,41 @@ export default function ContactUs() {
     const { data, title } = useAppSelector(state => state.counter);
 
     return (
-        <>
-
-            <Typography variant="h2">
-                {title}
-            </Typography>
-
-            <Typography variant="h5">
-                the data is {data}
-            </Typography>
-            <ButtonGroup>
-                <Button onClick={() => dispatch(decrement())} variant="contained" color="error">Decrement</Button>
-                <Button onClick={() => dispatch(increment(1))} variant="contained" color="primary">Increment</Button>
-                <Button onClick={() => dispatch(increment(5))} variant="contained" color="secondary">Increment by 5</Button>
-            </ButtonGroup>
-        </>
-
-    )
+<div>
+  {/* Created By CodingLab - www.codinglabweb.com */}
+  <meta charSet="UTF-8" />
+  {/* Fontawesome CDN Link */}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+  <meta name="viewport"  />
+  <style dangerouslySetInnerHTML={{__html: "\n       /* Google Font CDN Link */\n@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');\n*{\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box;\n  font-family: \"Poppins\" , sans-serif;\n}\nbody{\n  min-height: 100vh;\n  width: 100%;\n  background: #F0F0F0	;\n  align-items: center;\n  justify-content: center;\n}\n.container{\n  width: 85%;\n  background: #fff;\n  border-radius: 6px;\n  padding: 20px 60px 30px 40px;\n  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);\n}\n.container .content{\n  display: flex;\n  align-items: center;\n  justify-content: space-between;\n}\n.container .content .left-side{\n  width: 25%;\n  height: 100%;\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  justify-content: center;\n  margin-top: 15px;\n  position: relative;\n}\n.content .left-side::before{\n  content: '';\n  position: absolute;\n  height: 70%;\n  width: 2px;\n  right: -15px;\n  top: 50%;\n  transform: translateY(-50%);\n  background: #afafb6;\n}\n.content .left-side .details{\n  margin: 14px;\n  text-align: center;\n}\n.content .left-side .details i{\n  font-size: 30px;\n  color: #006CFF	;\n  margin-bottom: 10px;\n}\n.content .left-side .details .topic{\n  font-size: 18px;\n  font-weight: 500;\n}\n.content .left-side .details .text-one,\n.content .left-side .details .text-two{\n  font-size: 14px;\n  color: #afafb6;\n}\n.container .content .right-side{\n  width: 75%;\n  margin-left: 75px;\n}\n.content .right-side .topic-text{\n  font-size: 23px;\n  font-weight: 600;\n  color: #006CFF;\n}\n.right-side .input-box{\n  height: 50px;\n  width: 100%;\n  margin: 12px 0;\n}\n.right-side .input-box input,\n.right-side .input-box textarea{\n  height: 100%;\n  width: 100%;\n  border: none;\n  outline: none;\n  font-size: 16px;\n  background: #F0F1F8;\n  border-radius: 6px;\n  padding: 0 15px;\n  resize: none;\n}\n.right-side .message-box{\n  min-height: 110px;\n}\n.right-side .input-box textarea{\n  padding-top: 6px;\n}\n.right-side .button{\n  display: inline-block;\n  margin-top: 12px;\n}\n.right-side .button input[type=\"button\"]{\n  color: #fff;\n  font-size: 18px;\n  outline: none;\n  border: none;\n  padding: 8px 16px;\n  border-radius: 6px;\n  background: #3e2093;\n  cursor: pointer;\n  transition: all 0.3s ease;\n}\n.button input[type=\"button\"]:hover{\n  background: #5029bc;\n}\n\n@media (max-width: 950px) {\n  .container{\n    width: 90%;\n    padding: 30px 40px 40px 35px ;\n  }\n  .container .content .right-side{\n   width: 75%;\n   margin-left: 55px;\n}\n}\n@media (max-width: 820px) {\n  .container{\n    margin: 40px 0;\n    height: 100%;\n  }\n  .container .content{\n    flex-direction: column-reverse;\n  }\n .container .content .left-side{\n   width: 100%;\n   flex-direction: row;\n   margin-top: 40px;\n   justify-content: center;\n   flex-wrap: wrap;\n }\n .container .content .left-side::before{\n   display: none;\n }\n .container .content .right-side{\n   width: 100%;\n   margin-left: 0;\n }\n}\n\n\n   " }} />
+  <div className="container">
+    <div className="content">
+      <div className="left-side">
+        <div className="address details">
+          <i className="fas fa-map-marker-alt" />
+          <div className="topic">Address</div>
+          <div className="text-one">San Angel 240, Valle San Agustín,</div>
+          <div className="text-two"> 25215 Saltillo, Coah.</div>
+        </div>
+        <div className="phone details">
+          <i className="fas fa-phone-alt" />
+          <div className="topic">Phone</div>
+          <div className="text-one">8441254698</div>
+          <div className="text-two">8445614799</div>
+        </div>
+        <div className="email details">
+          <i className="fas fa-envelope" />
+          <div className="topic">Email</div>
+          <div className="text-one">joseG1@hexaware.com</div>
+          <div className="text-two">julissaQ@hexaware.com</div>
+        </div>
+      </div>
+      <div className="right-side">
+        <div className="topic-text">Send us a message</div>
+        <p>Please feel free to contact usif there is any problem with your order or if you need something else.</p>
+      </div>
+    </div>
+  </div>
+</div>
+      );
 }
