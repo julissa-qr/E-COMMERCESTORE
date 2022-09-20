2da072c8629f938a5e920eb1ac2e3db71b223559
Julissa Quiroz <julissa_qr@outlook.es>
2022-09-20 01:27:26 -0500

    ver ordenes OK

---
+++ store/src/app/layout/PrivateRouteOrder.tsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router";
+import Orders from "../features/orders/Order";
+import { useAppSelector } from "../store/configureStore";
+
+
+const PrivateRouteOrder = () => {
+    const { user } = useAppSelector(state => state.account);
+    return user ? <Orders />: <Navigate to="/login" />
+}
+
+export default PrivateRouteOrder;
